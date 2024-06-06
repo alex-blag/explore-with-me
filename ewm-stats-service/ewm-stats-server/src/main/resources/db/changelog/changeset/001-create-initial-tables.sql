547265aac5d1b5aa15eaa77ddad55721defe9843
Ab <104671142+alex-blag@users.noreply.github.com>
2024-06-06 08:47:33 +0300

    feat: add Liquibase

---
+++ ewm-stats-service/ewm-stats-server/src/main/resources/db/changelog/changeset/001-create-initial-tables.sql
@@ -0,0 +1,20 @@
+--
+
+CREATE TABLE IF NOT EXISTS service_apps (
+    id      BIGINT          GENERATED ALWAYS AS IDENTITY,
+    name    VARCHAR(250)    NOT NULL,
+
+    CONSTRAINT pk_service_app PRIMARY KEY (id),
+    CONSTRAINT uq_service_app UNIQUE (name)
+);
+
+CREATE TABLE IF NOT EXISTS endpoint_hits (
+    id          BIGINT          GENERATED ALWAYS AS IDENTITY,
+    app_id      BIGINT          NOT NULL,
+    uri         VARCHAR(250)    NOT NULL,
+    ip4         VARCHAR(15)     NOT NULL,
+    time_stamp  TIMESTAMP       NOT NULL,
+
+    CONSTRAINT pk_endpoint_hits PRIMARY KEY (id),
+    CONSTRAINT fk_endpoint_hits__service_apps FOREIGN KEY (app_id) REFERENCES service_apps(id) ON DELETE CASCADE
+);
