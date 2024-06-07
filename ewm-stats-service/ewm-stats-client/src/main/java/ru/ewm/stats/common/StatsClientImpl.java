77e8ce266c3b0f05c9baabf650482b783d5807aa
Ab <104671142+alex-blag@users.noreply.github.com>
2024-06-07 20:31:21 +0300

    feat: add StatsClient

--- ewm-stats-service/ewm-stats-client/src/main/java/ru/ewm/stats/common/StatsClientImpl.java
+++ ewm-stats-service/ewm-stats-client/src/main/java/ru/ewm/stats/common/StatsClientImpl.java
@@ -7,15 +7,20 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriComponentsBuilder;
 import ru.ewm.stats.common.dto.EndpointHitRequestDto;
 import ru.ewm.stats.common.dto.EndpointHitResponseDto;
 import ru.ewm.stats.common.dto.ViewStatsResponseDto;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class StatsClientImpl implements StatsClient {
+
+    public static final String HIT_ENDPOINT = "/hit";
+    public static final String STATS_ENDPOINT = "/stats";
 
     private final RestTemplate restTemplate;
 
@@ -31,14 +36,28 @@ public class StatsClientImpl implements StatsClient {
 
     @Override
     public EndpointHitResponseDto saveHit(EndpointHitRequestDto endpointHitRequestDto) {
-        throw new UnsupportedOperationException();
+        return restTemplate.postForObject(HIT_ENDPOINT, endpointHitRequestDto, EndpointHitResponseDto.class);
     }
 
     @Override
     public List<ViewStatsResponseDto> getStats(
             LocalDateTime begin, LocalDateTime end, List<String> uris, boolean unique
     ) {
-        throw new UnsupportedOperationException();
+        URI uri = UriComponentsBuilder
+                .fromPath(STATS_ENDPOINT)
+                .queryParam("begin", begin)
+                .queryParam("end", end)
+                .queryParam("uris", uris)
+                .queryParam("unique", unique)
+                .encode()
+                .build()
+                .toUri();
+
+        ViewStatsResponseDto[] viewStats = restTemplate.getForObject(uri, ViewStatsResponseDto[].class);
+
+        return viewStats == null
+                ? List.of()
+                : List.of(viewStats);
     }
 
 }
