4f419ce358d6e2b40587808bb030ebd60de85089
Ab <104671142+alex-blag@users.noreply.github.com>
2024-06-16 15:48:41 +0300

    refactor: add constructor to Common dtos

--- ewm-stats-service/ewm-stats-common/src/main/java/ru/ewm/stats/dto/EndpointHitRequestDto.java
+++ ewm-stats-service/ewm-stats-common/src/main/java/ru/ewm/stats/dto/EndpointHitRequestDto.java
@@ -1,12 +1,16 @@
 package ru.ewm.stats.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EndpointHitRequestDto {
 
     @NotBlank
